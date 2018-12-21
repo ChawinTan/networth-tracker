b6addc9a352c0403f9028db4c86c2ec3f5714369
ChawinTan <chawin15@gmail.com>
2018-12-21 14:03:39 +0800

    Initialize apis to connect server to database

--- networth-server/index.js
+++ networth-server/index.js
@@ -1,12 +1,24 @@
 var connection = require('./DbFields');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 connection.connect(function(err) {
-    if (err) throw err
-    console.log('You are now connected...')
+    if (err) throw err;
+    console.log('You are now connected...');
   });
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/signup', (req, res) => {
+
+    let insertUser = 'INSERT INTO users (email, user_password) VALUES (?,?);';
+    let query = connection.query(insertUser, [req.body.email, req.body.user_password], ( err, result ) => {
+        if (err) throw err;
+        res.send('User successfully signed up');
+    });
+});
 
  var server = app.listen(8081, function () {
     var host = server.address().address
