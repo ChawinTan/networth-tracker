a73c06f5c66dcb420a9a56e92e971643910e6965
ChawinTan <chawin15@gmail.com>
2018-12-22 23:24:20 +0800

    user signin api

--- networth-server/User/user.js
+++ networth-server/User/user.js
@@ -6,7 +6,7 @@ router.post('/signup', (req, res) => {
 
     let checkUserExist = 'SELECT COUNT(*) AS cnt FROM users WHERE email = ?';
     connection.query(checkUserExist, req.body.email, (err, data) => {
-        if (err) console.log(err);
+        if (err) throw(err);
         else {
             if (data[0].cnt > 0) {
                 res.send("Email already exist");
@@ -16,6 +16,20 @@ router.post('/signup', (req, res) => {
                     if (err) throw err;
                     res.send('User successfully signed up');
                 });
+            }
+        }
+    });
+});
+
+router.post('/signin', (req, res) => {
+    let retrieveUser = 'SELECT COUNT(*) AS cnt FROM users WHERE email = ? AND user_password = ?';
+    connection.query(retrieveUser, [req.body.email, req.body.user_password], (err, data) => {
+        if (err) throw(err);
+        else {
+            if (data[0].cnt === 1) {
+                res.send('Sign in success');
+            } else if (data[0].cnt === 0) {
+                res.send('Wrong credentials');
             }
         }
     });
