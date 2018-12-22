0b86cdf68cc6fee121720da529e4a19c8daf0d79
ChawinTan <chawin15@gmail.com>
2018-12-22 10:39:56 +0800

    check if user already exist

--- networth-server/User/user.js
+++ networth-server/User/user.js
@@ -4,10 +4,20 @@ var router = express.Router();
 
 router.post('/signup', (req, res) => {
 
-    let insertUser = 'INSERT INTO users (email, user_password) VALUES (?,?);';
-    let query = connection.query(insertUser, [req.body.email, req.body.user_password], ( err, result ) => {
-        if (err) throw err;
-        res.send('User successfully signed up');
+    let checkUserExist = 'SELECT COUNT(*) AS cnt FROM users WHERE email = ?';
+    connection.query(checkUserExist, req.body.email, (err, data) => {
+        if (err) console.log(err);
+        else {
+            if (data[0].cnt > 0) {
+                res.send("Email already exist");
+            } else {
+                let insertUser = 'INSERT INTO users (email, user_password) VALUES (?,?);';
+                connection.query(insertUser, [req.body.email, req.body.user_password], ( err, result ) => {
+                    if (err) throw err;
+                    res.send('User successfully signed up');
+                });
+            }
+        }
     });
 });
 
