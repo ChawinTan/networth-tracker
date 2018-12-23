e0e0eaf584437845afcc7e118c510b6dd77cfecf
ChawinTan <chawin15@gmail.com>
2018-12-23 15:16:47 +0800

    Banner heading

--- networth-view/src/App/App.js
+++ networth-view/src/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
+import Banner from '../Banner/Banner';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       App works!
+        <Banner />
       </div>
     );
   }
---
+++ networth-view/src/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Banner.css';
+
+class Banner extends Component {
+    render() {
+        return (
+            <div className='banner'>
+                <h1>How much are you worth?</h1>
+            </div>
+        );
+    }
+}
+
+export default Banner;
