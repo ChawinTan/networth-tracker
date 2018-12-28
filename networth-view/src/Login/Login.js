e175f96bd6c56c062afb599a360540f7432d3d81
ChawinTan <chawin15@gmail.com>
2018-12-28 13:56:06 +0800

    set up login function

--- networth-view/src/Login/Login.js
+++ networth-view/src/Login/Login.js
@@ -7,34 +7,42 @@ class Login extends Component {
         super(props);
 
         this.state = {
-            email: '',
-            password: '',
+            userEmail: '',
+            userPassword: '',
             validateLogin: true
         };
 
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
+        this.userLogin = this.userLogin.bind(this);
     }
 
     onChangeEmail(event) {
-        this.setState({ email: event.target.value });
+        this.setState({ userEmail: event.target.value });
     }
 
     onChangePassword(event) {
-        this.setState({ password: event.target.password });
+        this.setState({ userPassword: event.target.value });
     }
 
     userLogin() {
-        
+        const email = this.state.userEmail;
+        const password = this.state.userPassword;
+
+        if ( !email || !password ) {
+            this.setState({ validateLogin: false });
+        } else {
+            
+        }
     }
 
     render() {
         return (
             <div className='loginform'>
                 <div className='wrapper'>
-                    <input onChange={this.onChangeEmail} type='email' placeholder='Enter your Email ...'/>
-                    <input onChange={this.onChangePassword} type='password' placeholder='Enter your Password ...'/>
-                    <button className='login-button'>Login</button>
+                    <input id='email' onChange={this.onChangeEmail} type='email' placeholder='Enter your Email ...'/>
+                    <input id='password' onChange={this.onChangePassword} type='password' placeholder='Enter your Password ...'/>
+                    <button onClick={this.userLogin} className='login-button'>Login</button>
                     { !this.state.validateLogin ?
                         <p>Please enter the correct email or password.</p> : null
                     }
