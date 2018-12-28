5f41b972aa3718a4bb59ec673f12137c8b61a447
ChawinTan <chawin15@gmail.com>
2018-12-28 14:23:02 +0800

    enable cors

--- networth-server/index.js
+++ networth-server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var userRoutes = require('./User/user');
 
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
