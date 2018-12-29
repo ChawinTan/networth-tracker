52fb359b90160dd108698e561e052df972f6958c
ChawinTan <chawin15@gmail.com>
2018-12-29 19:08:24 +0800

    Sign up form design

--- networth-view/src/SignUp/SignUp.js
+++ networth-view/src/SignUp/SignUp.js
@@ -5,8 +5,13 @@ import './SignUp.css';
 class SignUp extends Component {
     render() {
         return (
-            <div>
-                <p>Sign Up works!</p>
+            <div className='signupform'>
+                <div className='wrapper'>
+                    <input type='email' placeholder='Enter your prefered email ...' />
+                    <input type='password' placeholder='Enter your prefered password ...' />
+                    <input type='password' placeholder='Confirm password ...' />
+                    <button className='signup-button'>Sign Me Up!</button>
+                </div>
             </div>
         );
     }
