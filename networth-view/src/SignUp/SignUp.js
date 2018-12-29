6aa3e0172b365466d99fe140be02449a262e133b
ChawinTan <chawin15@gmail.com>
2018-12-29 19:36:27 +0800

    user inputs

--- networth-view/src/SignUp/SignUp.js
+++ networth-view/src/SignUp/SignUp.js
@@ -3,13 +3,43 @@ import React, { Component } from 'react';
 import './SignUp.css';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userEmail: '',
+            userPassword: '',
+            confirmPassword: '',
+            validateEmail: true,
+            validatePassword: true
+        }
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeConfirmPassword = this.onChangeConfirmPassword.bind(this);
+    }
+
+    onChangeEmail(event) {
+        this.setState({ userEmail: event.target.value });
+        console.log(this.state.userEmail)
+    }
+
+    onChangePassword(event) {
+        this.setState({ userPassword: event.target.value });
+        console.log(this.state.userPassword)
+    }
+
+    onChangeConfirmPassword(event) {
+        this.setState({ confirmPassword: event.target.value });
+        console.log(this.state.confirmPassword)
+    }
+
     render() {
         return (
             <div className='signupform'>
                 <div className='wrapper'>
-                    <input type='email' placeholder='Enter your prefered email ...' />
-                    <input type='password' placeholder='Enter your prefered password ...' />
-                    <input type='password' placeholder='Confirm password ...' />
+                    <input onChange={this.onChangeEmail} type='email' placeholder='Enter your prefered email ...' />
+                    <input onChange={this.onChangePassword} type='password' placeholder='Enter your prefered password ...' />
+                    <input onChange={this.onChangeConfirmPassword} type='password' placeholder='Confirm password ...' />
                     <button className='signup-button'>Sign Me Up!</button>
                 </div>
             </div>
