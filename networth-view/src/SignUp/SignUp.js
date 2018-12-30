12dd26b3ff7e58919c9526d0f0e75e26766537b8
ChawinTan <chawin15@gmail.com>
2018-12-30 11:59:56 +0800

    validate email and password

--- networth-view/src/SignUp/SignUp.js
+++ networth-view/src/SignUp/SignUp.js
@@ -16,21 +16,37 @@ class SignUp extends Component {
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.onChangeConfirmPassword = this.onChangeConfirmPassword.bind(this);
+        this.onClickSignUp = this.onClickSignUp.bind(this);
     }
 
     onChangeEmail(event) {
         this.setState({ userEmail: event.target.value });
-        console.log(this.state.userEmail)
     }
 
     onChangePassword(event) {
         this.setState({ userPassword: event.target.value });
-        console.log(this.state.userPassword)
     }
 
     onChangeConfirmPassword(event) {
         this.setState({ confirmPassword: event.target.value });
-        console.log(this.state.confirmPassword)
+    }
+
+    onClickSignUp() {
+        const email = this.state.userEmail;
+        const password = this.state.userPassword;
+        const checkPassword = this.state.confirmPassword;
+
+        if (!email) {
+            this.setState({ validateEmail: false });
+        }
+
+        if (password !== checkPassword) {
+            this.setState({ validatePassword: false });
+        }
+
+        else {
+            
+        }
     }
 
     render() {
@@ -38,9 +54,11 @@ class SignUp extends Component {
             <div className='signupform'>
                 <div className='wrapper'>
                     <input onChange={this.onChangeEmail} type='email' placeholder='Enter your prefered email ...' />
+                    {!this.state.validateEmail ? <p>Email cannot be empty!</p> : null}
                     <input onChange={this.onChangePassword} type='password' placeholder='Enter your prefered password ...' />
                     <input onChange={this.onChangeConfirmPassword} type='password' placeholder='Confirm password ...' />
-                    <button className='signup-button'>Sign Me Up!</button>
+                    {!this.state.validatePassword ? <p>Passwords do not match!</p> : null}
+                    <button onClick={this.onClickSignUp} className='signup-button'>Sign Me Up!</button>
                 </div>
             </div>
         );
