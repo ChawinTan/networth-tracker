f018e1c1227bec52721624c50acb8a65111e17a5
ChawinTan <chawin15@gmail.com>
2019-02-23 11:30:32 +0800

    chart complete, MVP!

--- networth-view/src/DashBoard/Dashboard.js
+++ networth-view/src/DashBoard/Dashboard.js
@@ -5,11 +5,6 @@ import './Dashboard.css';
 
 class Dashboard extends Component {
 
-    componentWillMount() {
-        console.log(this.getCategories(this.props.networth));
-        console.log(this.buildChart());
-    }
-
     getCategories(networth) {
         return networth.map(entry => {
             const date = new Date(entry.entry_date);
@@ -25,26 +20,111 @@ class Dashboard extends Component {
         });
     }
 
-    buildChart() {
+    getCash(networth) {
+        return networth.map(entry => {
+            return parseFloat(entry.cash);
+        });
+    }
+
+    getInvestments(networth) {
+        return networth.map(entry => {
+            return parseFloat(entry.investments);
+        });
+    }
+
+    getOtherAssets(networth) {
+        return networth.map(entry => {
+            return parseFloat(entry.other_assets);
+        });
+    }
+
+    buildTrendChart() {
         return {
+            chart: {
+                type: 'line',
+                zoomType: 'x'
+            },
             title: {
                 text: 'Networth Trend'
             },
             xAxis: {
+                title: {
+                    text: 'Date'
+                },
                 categories: this.getCategories(this.props.networth)
             },
+            yAxis: {
+                title: {
+                    text: 'Networth'
+                }
+            },
             series: [{
+                    name: 'Networth Trend Line',
                     data: this.getTotal(this.props.networth)
                 }]
         };
     }
 
+    buildBreakdownChart() {
+        return {
+            chart: {
+                type: 'column',
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Networth Breakdown'
+            },
+            xAxis: {
+                title: {
+                    text: 'Date'
+                },
+                categories: this.getCategories(this.props.networth)
+            },
+            yAxis: {
+                title: {
+                    text: 'Networth Breakdown'
+                }
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal'
+                }
+            },
+            series:[
+                {
+                    name: 'Cash',
+                    data: this.getCash(this.props.networth)
+                },
+                {
+                    name: 'Investments',
+                    data: this.getInvestments(this.props.networth)
+                },
+                {
+                    name: 'Other Assets',
+                    data: this.getOtherAssets(this.props.networth)
+                }
+            ]
+        }
+    }
+
     render() {
         return (
-            <div className="dashboard">
-                <HighchartsReact
-                config={this.buildChart()}
-                 />
+            <div>
+                {this.props.networth.length > 0 ?
+                <div className="dashboard">
+                    <div>
+                        <HighchartsReact
+                        config={this.buildTrendChart()}
+                        />
+                    </div>
+                    <div>
+                        <HighchartsReact
+                        config={this.buildBreakdownChart()}
+                        />
+                    </div>
+                </div> :
+                <div className="noChart">No chart to display yet</div>
+                }
             </div>
         );
     }
